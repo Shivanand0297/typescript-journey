dca31ead174f0dfc8db14aa7f8faf016f8d574df
shivanand <shivanand02071997@gmail.com>
2023-07-03 18:44:45 +0530

    writing sorting logic - bad code

--- 04-sort/build/index.js
+++ 04-sort/build/index.js
@@ -1,2 +1,27 @@
 "use strict";
-console.log("555");
+class Sorter {
+    constructor(collection) {
+        this.collection = collection;
+    }
+    sort() {
+        const length = this.collection.length;
+        // sorting for numbers array
+        if (this.collection instanceof Array) {
+            for (let i = 0; i < length; i++) {
+                for (let j = 0; j < length - i - 1; j++) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        let temp = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = temp;
+                    }
+                }
+            }
+        }
+        if (typeof this.collection === "string") {
+            // sorting for string 
+        }
+    }
+}
+const sorter = new Sorter([32, -2, 0, 77]);
+sorter.sort();
+console.log(sorter.collection);
